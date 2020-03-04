17b0723fffec1405259056d46a0564d324c35d57
Luiza Carvalho <luiza177music@gmail.com>
2020-03-04 17:38:03 +0100

    added thresh + notes

--- loudnessnorm.sh
+++ loudnessnorm.sh
@@ -23,21 +23,27 @@ for file in $files; do
     mkdir "${output_folder}"
   fi
 
-  # TODO: think about what to do with dual_mono
+  
+
   ffmpeg -i "$file" -hide_banner -af loudnorm=I=-16:TP=-3.0:dual_mono=true:print_format=summary -f null - 2> "$temp_file"
 
+  # TODO: if needed convert to wav: ffmpeg -i input.m4a output.wav
+  # TODO: if extension M4A, set -ab 128
+  
   # echo "  Input:"
 
   integrated="$(awk '/Input Integrated:/ { print $3 }' "$temp_file")"
   lra="$(awk '/Input LRA:/ { print $3 }' "$temp_file")"
   truepeak="$(awk '/Input True Peak:/ { print $4 }' "$temp_file")"
+  thresh="$(awk '/Input Threshold:/ { print $3 }' "$temp_file")"
   
   # echo -e "    Integrated  =  ${integrated} LUFS"
   # echo -e "    True Peak   =  ${truepeak} dBTP"
   # echo -e "    LRA         =   ${lra}"
 
-  ffmpeg -i "$file" -hide_banner -loglevel warning -af "loudnorm=I=-16:TP=-3.0:dual_mono=true:measured_I=${integrated}:measured_TP=${truepeak}:measured_LRA=${lra}:offset=-0.3:linear=true:print_format=summary" -ar 44100 "$output"  #2> "$temp_file"
-
+  # ffmpeg -i "$file" -hide_banner -loglevel warning -af "loudnorm=I=-16:TP=-3.0:dual_mono=true:measured_I=${integrated}:measured_TP=${truepeak}:measured_LRA=${lra}:offset=-0.3:linear=true:print_format=summary" -ar 44100 "$output"  #2> "$temp_file"
+  ffmpeg -i "$file" -hide_banner -loglevel warning -af "loudnorm=I=-16:TP=-3.0:dual_mono=true:measured_I=${integrated}:measured_TP=${truepeak}:measured_LRA=${lra}:measured_thresh=${thresh}:linear=true:print_format=summary" -ar 44100 "$output"  #2> "$temp_file"
+  
   # echo "  Output:"
 
   rm "$temp_file"
