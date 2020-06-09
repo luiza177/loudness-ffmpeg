8a124aea6bf975d6abf03af30a7b7ed8cb616e36
Luiza Carvalho <luiza177music@gmail.com>
2020-06-09 12:02:57 +0200

    clearer mono handling, no empty folders

--- loudnessnorm.sh
+++ loudnessnorm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TODO: --option for copying to folder even if not normalized
+# TODO: --option to specify output folder
+# TODO: handle multiple folders
+
+
 OUTPUT_FOLDER_NAME="Normalized"
 
 SAVEIFS="${IFS}"
@@ -26,30 +31,39 @@ for file in $files; do
 
   ffmpeg -i "$file" -hide_banner -af loudnorm=I=-16:TP=-3.0:dual_mono=true:print_format=summary -f null - 2> "$temp_file"
 
-  # echo "  Input:"
-
   integrated="$(awk '/Input Integrated:/ { print $3 }' "$temp_file")"
   lra="$(awk '/Input LRA:/ { print $3 }' "$temp_file")"
   truepeak="$(awk '/Input True Peak:/ { print $4 }' "$temp_file")"
   thresh="$(awk '/Input Threshold:/ { print $3 }' "$temp_file")"
-  
-  # echo -e "    Integrated  =  ${integrated} LUFS"
-  # echo -e "    True Peak   =  ${truepeak} dBTP"
+  channels="$(awk -F', ' '/Stream #0:0/ { print $3; exit }' "$temp_file")"
+
+  if [ "$channels" == "mono" ]; then
+    echo -e "    Integrated  =  ${integrated} LUFS (dual mono)"
+  else
+    echo -e "    Integrated  =  ${integrated} LUFS"
+  fi 
+  echo -e "    True Peak   =  ${truepeak} dBTP"
   # echo -e "    LRA         =   ${lra}"
 
   # bc -l returns 0 or 1
-  if [ $(echo "$integrated > -15.3" | bc -l) -eq 1 ] || [ $(echo  "$integrated < -16.8" | bc -l) -eq 1 ] || [ $(echo  "$truepeak > -2.5" | bc -l) -eq 1 ]; then 
+  if [ $(echo "$integrated > -15.1" | bc -l) -eq 1 ] || [ $(echo  "$integrated < -16.9" | bc -l) -eq 1 ] || [ $(echo  "$truepeak > -2.5" | bc -l) -eq 1 ]; then 
     COMMAND="ffmpeg -i \"$file\" -hide_banner -loglevel warning -af \"loudnorm=I=-16:TP=-3.0:dual_mono=true:measured_I=${integrated}:measured_TP=${truepeak}:measured_LRA=${lra}:measured_thresh=${thresh}:linear=true:print_format=summary\" -ar 44100 "
     if [ ${file:(-3)} == "m4a" ]; then 
       COMMAND+="-ab 128000 "
     fi
-    COMMAND+="$output"
+    COMMAND+="\"$output\""
     # ffmpeg -i "$file" -hide_banner -loglevel warning -af "loudnorm=I=-16:TP=-3.0:dual_mono=true:measured_I=${integrated}:measured_TP=${truepeak}:measured_LRA=${lra}:measured_thresh=${thresh}:linear=true:print_format=summary" -ar 44100 "$output"  #2> "$temp_file"
     eval "$COMMAND" 
+    echo -e "    --> done."
   else
-    echo "${base} is already at ${integrated} LUFS and ${truepeak} dBTP"
-    # cp "$file" "$output"
+    echo "    --> ${base} is already within tolerance of -16 LUFS (+/-1 LU) and -3.0 (+0.5) dBTP"
+    #? cp "$file" "$output"
   fi
 
   rm "$temp_file"
+  # removes empty folder
+  if [ -z "$(ls ${output_folder})"]; then
+    rm -r "$output_folder"
+  fi
+
 done
